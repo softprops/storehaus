f23c081f109eac364c2e034bbb6bd45c114a66bc
softprops <d.tangren@gmail.com>
2013-04-26 07:55:47 -0400

    store with exists semantics

---
+++ storehaus-zookeeper/src/main/scala/com/twitter/storehaus/zookeeper/ExistenceStore.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 Twitter Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.twitter.storehaus.zookeeper
+
+import com.twitter.util.Future
+import com.twitter.storehaus.ReadableStore
+import com.twitter.zk.{ ZkClient, ZNode }
+import org.apache.zookeeper.data.Stat
+
+/**
+ *  @author Doug Tangren
+ */
+
+object ExistenceStore {
+  def apply(client: ZkClient) =
+    new ExistenceStore(client)
+}
+
+class ExistenceStore(val client: ZkClient)
+  extends ReadableStore[String, Stat] {
+
+  override def get(k: String): Future[Option[Stat]] =
+    client(k).sync.flatMap { node =>
+      node.exists().map(e => Option(e.stat)).handle({
+        case ZNode.Error(_) => None
+      })
+    }
+
+  override def close { client.release }
+}
+
